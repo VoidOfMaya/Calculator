38481234652b6709b4240ed9a11489c7b7f28ad4
VoidOfMaya <mayaofthevoid@gmail.com>
2023-04-22 17:36:01 +0300

    create css,js,html files + create basic math functions

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 this project is the final project in the Odin Projects fundamentals portion of the course!
 this project will incorporate html, css, javascript 
+(using the "eval()" function is not allowed due to security riskes assocciated with eval().)
---
+++ index.js
@@ -0,0 +1,22 @@
+// creating the basic functions for simple math
+  // key note for futer referance, calculation functions take  variables of  type Number
+
+function add(numberA , numberB){
+    return (numberA + numberB);
+}
+
+function subtract(numberA,numberB){
+    return (numberA - numberB);
+}
+function multiply(numberA,numberB){
+    return (numberA * numberB);
+}
+function divide(numberA,numberB){
+    return (numberA / numberB);
+}
+
+
+console.log(add(2 , 10));
+console.log(subtract(2 , 10));
+console.log(multiply(2 , 10));
+console.log(divide(2 , 10));
