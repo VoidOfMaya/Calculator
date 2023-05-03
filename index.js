9a44d9197e94a332d376ac5bd870007077794a29
VoidOfMaya <mayaofthevoid@gmail.com>
2023-05-03 15:46:25 +0300

    enable basic non recursive calculator functionality

--- index.js
+++ index.js
@@ -1,81 +1,54 @@
 // creating the basic functions for simple math
   // key note for futer referance, calculation functions take  variables of  type Number
 
-function add(numberA , numberB){
-    return Number(numberA + numberB);
-}
-function subtract(numberA,numberB){
-    return Number(numberA - numberB);
-}
-function multiply(numberA,numberB){
-    return Number(numberA * numberB);
-}
-function divide(numberA,numberB){
-    return Number(numberA / numberB);
-}
-
-
-function operate(numberA , operator , numberB){
-    switch (operator) {
-        case '+':
-            return add(numberA,numberB);
-
-        case '-':        
-            return subtract(numberA,numberB);
-     
-        case '*':
-            return multiply(numberA,numberB);
-
-        case '/':
-            return divide(numberA,numberB);
-    
-        default:
-            break;
-    }
-
-}
 
 function populateDisplay(){
     
-    const display = document.querySelector('.display');
-    const buttons = document.querySelectorAll('.row button');
-    const displayArray =[];
-    let bufferArray =[];
-    let chainNumber =0;
-    let firstNumber =0;
-    let lastNumber =0;
-    let choosenOperator;
-    buttons.forEach((btn) => btn.addEventListener('click', () =>{
-        
-        //display content on screen
-        displayArray.push(btn.id);
-        display.textContent= displayArray.join('');
+    const currentDisNumber = document.querySelector('.main-display');
+    const lastDisNumber= document.querySelector('.upper-display');
 
-        //populate relevant variables required for the operate function
-        if( btn.className ==='digit'){
-            bufferArray.push(btn.id);
+    const number = document.querySelectorAll('.digit');
+    const operators = document.querySelectorAll('.operator');
+    const equal = document.querySelector('.equal');
+    equal.addEventListener('click', operate);
+    let currentNum= '';
+    let lastNum='';
+    let operator;
+    
+    number.forEach((btn) => btn.addEventListener('click', () =>{
+
+        if (currentNum.length <= 12){
+            currentNum +=btn.id;
+            console.log(`${currentNum}`);
+            currentDisNumber.textContent = currentNum;    
         }
-        if (choosenOperator !== 'undefined' && btn.className === 'operator'){
-            //assignes an operator to a variable
-            choosenOperator = btn.id;
-
-            // proccesses numebrs asigned to buffer and pushes them inside variables + calls the operate function
-            firstNumber === 0? firstNumber = bufferArray.join('') : lastNumber = bufferArray.join('');
-
-            bufferArray =[];
-            if (choosenOperator ==='='){
-
-                chainNumber = operate(firstNumber, choosenOperator,lastNumber);
-            }
         
-        }
-        //console.log(`${choosenOperator} is of type ${typeof choosenOperator}`);
-
-        console.log(` first = ${firstNumber} , operator = ${choosenOperator} , second = ${lastNumber} , buffer = ${bufferArray.join('')} \nchain number : ${chainNumber}`);
-        
-
     }))
     
+    operators.forEach((op) => op.addEventListener('click', ()=>{
+        operator = op.id;
+        lastDisNumber.textContent = currentNum + " " + operator;
+        lastNum = currentNum;
+        currentNum =''
+        currentDisNumber.textContent= '';
+
+        console.log(`last number: ${lastNum} operator: ${operator} current number: ${currentNum}`);
+    }))
+
+    
+
+    function operate(){
+        currentNum = Number(currentNum);
+        lastNum = Number(lastNum);
+
+        operator === '+'?lastNum += currentNum :lastNum;
+        operator === '-'?lastNum -= currentNum :lastNum;
+        operator === '*'?lastNum *= currentNum :lastNum;
+        operator === '/'?lastNum /= currentNum :lastNum;
+
+        lastDisNumber.textContent ='';
+        currentDisNumber.textContent= lastNum;
+    }
     
 
 }
